5554c5faed1bb1368ae149ef2af921fc676327cd
Manjun <42054560+Manjun-Wang@users.noreply.github.com>
2018-09-23 23:26:58 +0800

    Create setup.py
    update with setup.py to enable the command line function

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='code_examiner',
+      version='0.1',
+      description='A fun tool to check if your code base',
+      url='https://github.com/Manjun-Wang/code-examier-py',
+      author='Manjun',
+      author_email='',
+      license='MIT',
+      scripts=['bin/code_exam'],
+      packages=['code_examiner'],
+      zip_safe=False)
